86329f99d66c50f40dd270799289daf7b075a254
shiva <shivajreddy@outlook.com>
2024-12-04 14:17:46 -0500

    [wip] day3-part2

--- 2024/cpp/day-03/part-02/main.cpp
+++ 2024/cpp/day-03/part-02/main.cpp
@@ -13,10 +13,11 @@ using std::vector;
 #pragma endregion
 
 #pragma region Program-Constants
-const string FILE_PATH = "./2024/cpp/day-03/part-01/input.txt";
+// const string FILE_PATH = "./2024/cpp/day-03/part-02/test.txt";
+const string FILE_PATH = "./2024/cpp/day-03/part-02/input.txt";
 #pragma endregion
 
-struct MatchInfo
+struct Match1
 {
     string match;
     size_t start;
@@ -25,9 +26,45 @@ struct MatchInfo
     int y;
 };
 
-vector<MatchInfo> findRegexMatches(const string &text, const string &pattern)
+struct Match2
 {
-    vector<MatchInfo> matches;
+    size_t start;
+    size_t end;
+    bool isDo; // true for 'do()' false for 'don't()'
+};
+
+vector<Match2> findRegex2Matches(const string &text, const string &pattern)
+{
+    vector<Match2> result;
+    try {
+        // Create Regex object
+        std::regex re(pattern);
+
+        // Find all regex matches for given text
+        auto wordBegin = std::sregex_iterator(text.begin(), text.end(), re);
+        auto wordEnd = std::sregex_iterator();
+
+        // Iterate over the matches
+        for (std::sregex_iterator i = wordBegin; i != wordEnd; ++i) {
+            std::smatch match = *i;
+
+            size_t start = match.position();
+            size_t end = start + match.length();
+            bool isDo = match.str() == "do()";
+
+            Match2 reg2Match { start, end, isDo };
+            result.push_back(reg2Match);
+        }
+    }
+    catch (std::regex_error &error) {
+        cerr << "Reg2Match-ERROR: " << error.what() << endl;
+    }
+    return result;
+}
+
+vector<Match1> findRegex1Matches(const string &text, const string &pattern)
+{
+    vector<Match1> matches;
 
     try {
         // Create regex object
@@ -51,7 +88,7 @@ vector<MatchInfo> findRegexMatches(const string &text, const string &pattern)
             int y = std::stoi(match[2].str()); // Second capture group
 
             // Store match information
-            MatchInfo newMatch { fullMatch, start, end, x, y };
+            Match1 newMatch { fullMatch, start, end, x, y };
             matches.push_back(newMatch);
         }
     }
@@ -60,6 +97,72 @@ vector<MatchInfo> findRegexMatches(const string &text, const string &pattern)
     }
 
     return matches;
+}
+
+int computeDoAndDont(const vector<Match1> &regex1Matches,
+                     const vector<Match2> &regex2Matches)
+{
+    /*
+    for (auto &match : regex1Matches) {
+        cout << match.start << "|" << match.end << " [" << match.x << ","
+             << match.y << "]" << endl;
+    }
+    // cout << "allDoDontMatches.size()=" << regex2Matches.size() << endl;
+    for (auto &match : regex2Matches) {
+        cout << match.start << "|" << match.end << " [" << match.isDo << "]"
+             << endl;
+    }
+    */
+
+    int result { 0 };
+
+    // Add all the beginning mul() before do() | don't()
+    int matches1_idx = 0;
+    int matches2_idx = 0;
+
+    while (matches1_idx < regex1Matches.size() &&
+           regex1Matches[matches1_idx].end <
+               regex2Matches[matches2_idx].start) {
+        result +=
+            (regex1Matches[matches1_idx].x * regex1Matches[matches1_idx].y);
+        ++matches1_idx;
+    }
+    cout << "After all begining: result= " << result << endl;
+
+    // Skip all the matches1 until we are after match2-end
+    for (; matches2_idx < regex2Matches.size(); ++matches2_idx) {
+
+        const auto &match2 = regex2Matches[matches2_idx];
+        // cout << "curr match2-iter " << match2.start << "|" << match2.end << "
+        // ["
+        // << match2.isDo << "]" << endl;
+
+        // cout << "curr-match1-start " << regex1Matches[matches1_idx].start <<
+        // "|"
+        //      << regex1Matches[matches1_idx].end << " ["
+        //      << regex1Matches[matches1_idx].x << ","
+        //      << regex1Matches[matches1_idx].y << "]" << endl;
+
+        // Skip mul() that are before the do()/don't()
+        while (matches1_idx < regex1Matches.size() &&
+               match2.end > regex1Matches[matches1_idx].start) {
+            ++matches1_idx;
+        }
+        // cout << "curr-match1-end " << matches1_idx << " "
+        //      << regex1Matches[matches1_idx].start << "|"
+        //      << regex1Matches[matches1_idx].end << " ["
+        //      << regex1Matches[matches1_idx].x << ","
+        //      << regex1Matches[matches1_idx].y << "]" << endl;
+
+        if (match2.isDo && matches1_idx < regex1Matches.size()) {
+            result +=
+                (regex1Matches[matches1_idx].x * regex1Matches[matches1_idx].y);
+            ++matches1_idx; // Move to the next match1
+        }
+    }
+    cout << "LINE RESULT = " << result << endl;
+
+    return result;
 }
 
 int main()
@@ -76,22 +179,26 @@ int main()
     }
 
     // Regular Expression, that match mul(x,y)
-    string regularExpression = "mul\\((\\d{1,3}),\\s*(\\d{1,3})\\)";
+    string regularExpression = R"(mul\((\d{1,3}),(\d{1,3})\))";
+
+    string regularExpression2 = R"(do\(\)|don't\(\))";
 
     string line;
 
     int result { 0 };
 
     while (getline(file, line)) {
+        // cout << line << endl;
 
         // Create Pattern Matches
-        auto allMatches = findRegexMatches(line, regularExpression);
+        auto regex1Matches = findRegex1Matches(line, regularExpression);
 
-        for (auto &match : allMatches) {
-            result += (match.x * match.y);
-        }
+        auto regex2Matches = findRegex2Matches(line, regularExpression2);
+
+        result += computeDoAndDont(regex1Matches, regex2Matches);
     }
     cout << "RESULT: " << result << endl;
+    // 14871649 -> too low
 
     file.close();
 
